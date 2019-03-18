599a53c8625f06eda9f782576b4bef4612f3a108
Priyanka Mukund <priyanka@kunday.com>
2019-03-19 09:42:54 +1100

    Made changes in weather andgiphy file

--- lib/api/weather.rb
+++ lib/api/weather.rb
@@ -5,7 +5,29 @@ module Api
     def fetch(query:)
       geometry = Api::Geocoder.new.fetch(query: query)
       response = HTTParty.get("https://api.darksky.net/forecast/08b00281f472f76d5b144da2d53fa28c/#{geometry["lat"]},#{geometry["lng"]}?units=si")
-      
+      {
+        current_temperature: response["currently"]["temperature"],
+        time_zone: response["timezone"],
+        summary: response["currently"]["summary"],
+        emoticon: weather_emoticon(response)
+      } 
+    end
+
+    def weather_emoticon(response)
+      icons = {
+        "clear-day" => '🌞',
+        "clear-night" => '🌚',
+        "rain" => '🌧',
+        "snow" => '❄',
+        "sleet" => '🌨',
+        "wind" =>  '🌬',
+        "fog" =>  '🌫',
+        "cloudy" => '☁',
+        "partly-cloudy-day" =>  '🌤',
+        "partly-cloudy-night" => '⛅'
+      }
+      icon = response["currently"]["icon"]
+      icons[icon]
     end
   end
 end

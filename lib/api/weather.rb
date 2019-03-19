f7098fca5506a29ba051bd4ec48c0d6ab7a9e7b1
Priyanka Mukund <priyanka@kunday.com>
2019-03-19 14:39:34 +1100

    Made changes in weather app and movies.

--- lib/api/weather.rb
+++ lib/api/weather.rb
@@ -1,30 +1,36 @@
 require 'httparty'
+
 require_relative 'geocoder'
 module Api
   class Weather
     def fetch(query:)
       geometry = Api::Geocoder.new.fetch(query: query)
       response = HTTParty.get("https://api.darksky.net/forecast/08b00281f472f76d5b144da2d53fa28c/#{geometry["lat"]},#{geometry["lng"]}?units=si")
-      {
-        current_temperature: response["currently"]["temperature"],
-        time_zone: response["timezone"],
-        summary: response["currently"]["summary"],
-        emoticon: weather_emoticon(response)
-      } 
+      # binding.pry
+      # if geometry === ""
+      #  redirect_to "/home/index"
+      # else
+        {
+          current_temperature: response["currently"]["temperature"],
+          time_zone: response["timezone"],
+          summary: response["currently"]["summary"],
+          emoticon: weather_emoticon(response)
+        }
+      # end
     end
 
     def weather_emoticon(response)
       icons = {
-        "clear-day" => '🌞',
-        "clear-night" => '🌚',
-        "rain" => '🌧',
-        "snow" => '❄',
-        "sleet" => '🌨',
+        "clear-day" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/day.svg',
+        "clear-night" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/night.svg',
+        "rain" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/rainy-6.svg',
+        "snow" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/snowy-6.svg',
+        "sleet" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/snowy-5.svg',
         "wind" =>  '🌬',
-        "fog" =>  '🌫',
-        "cloudy" => '☁',
-        "partly-cloudy-day" =>  '🌤',
-        "partly-cloudy-night" => '⛅'
+        "fog" =>  'hzttps://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/cloudy.svg',
+        "cloudy" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/cloudy.svg',
+        "partly-cloudy-day" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/cloudy-day-1.svg',
+        "partly-cloudy-night" => 'https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/cloudy-night-3.svg'
       }
       icon = response["currently"]["icon"]
       icons[icon]
